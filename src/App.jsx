74ca22de903172a8833bd1f148dc1f233e7ba022
Tamim <tammu1000@gmail.com>
2024-01-21 01:06:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button.jsx";
+
+
+function App() {
+
+  return (
+      <div className="px-80">
+        <Button>Testing</Button>
+      </div>
+  )
+}
+
+export default App
