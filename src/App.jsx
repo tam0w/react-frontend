c7ef6b5a4d81c4d367de9e44b29718cbe59621ee
Tamim <tammu1000@gmail.com>
2024-01-21 01:23:59 +0530

    theming works!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import {Button} from "@/components/ui/button.jsx";
+import {TypographyDemo} from "@/demo/Typing.jsx";
+import {Slider} from "@/components/ui/slider.jsx";
 
 
 function App() {
 
   return (
-      <div className="px-80">
-        <Button>Testing</Button>
+      <div>
+          <div className="p-12 primary">
+            <TypographyDemo/>
+             <Slider className="bg-primary-foreground"></Slider>
+          </div>
+
       </div>
   )
 }
