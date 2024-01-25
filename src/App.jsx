7ab79509300ebfefe4576857b7df25f6a1cb89fe
Tamim <tammu1000@gmail.com>
2024-01-25 20:21:13 +0530

    new css classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {Navbar} from "@/demo/Navbar.jsx";
 import {Counter} from "@/demo/Counter.jsx";
 import {GetAPI} from "@/demo/GetAPI.jsx";
+import {Button} from "@/components/ui/button.jsx";
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
           <Navbar/>
           <Counter/>
               <GetAPI/>
+
           </div>
       </div>
   )
