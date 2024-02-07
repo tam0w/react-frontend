753fd01c62728d3239948759fdc1b15fde07ae5c
Tamim <tammu1000@gmail.com>
2024-02-07 22:25:39 +0530

    gotta make side bar item?

--- src/demo/Sidebar.jsx
+++ src/demo/Sidebar.jsx
@@ -1,14 +1,18 @@
-
 import {Link} from "react-router-dom";
 import{BellIcon,  DashboardIcon, DoubleArrowLeftIcon, HamburgerMenuIcon, SunIcon,} from "@radix-ui/react-icons";
-
+import {useState} from "react";
+import img1 from "@/assets/greensmol.png";
 export default function Component() {
+
+  const [expanded, setExpanded] = useState(true)
+
   return (
     <div className="flex h-screen">
       <aside className="w-72 bg-muted/50 font text-2xl text-card-foreground border-r-2 border-muted-foreground">
-        <div className="h-24 flex items-center space-x-9 justify-center border-muted-foreground">
-          <h2 className="font button-text text-3xl">Practistics</h2>
-          <DoubleArrowLeftIcon className="h-8 w-8 border-muted-foreground/30 rounded hover:text-blue-300" />
+        <div className="h-24 flex items-center space-x-2 justify-center border-muted-foreground">
+          <img src={img1} className={`h-12 overflow-hidden transition-all ${
+              expanded ? "w-56" : "w-0"
+            }`}></img>
         </div>
         <ul>
           <li className="flex items-center p-6 hover:bg-primary/20 hover:text-blue-100 hover:border-l-2">
